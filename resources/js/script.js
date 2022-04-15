dda79e46bae52506913c8647f7efb451af667e16
rbidon <roussebidon@gmail.com>
2022-04-15 00:33:31 -0400

    Added a home social media & push for saving reason

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,17 @@
 console.log("testing");
+// menu burger 
+const toggleButton = document.getElementById("toggleButton");
+const navLink = document.getElementById("navLink");
+toggleButton.addEventListener("click", () => {
+    navLink.classList.toggle('active');
+})
+
+// Project Section: tab
+const tabs = document.querySelectorAll('[data-tab-target');
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        // show target 
+        target.classList.add("active");
+    })
+})
