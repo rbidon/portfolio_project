c865bcc40384f496d744cc910ecb77e77be721be
rbidon <roussebidon@gmail.com>
2022-04-23 16:40:59 -0400

    Completed making form

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-console.log("testing");
+console.log("Working!");
 // menu burger 
 const toggleButton = document.getElementById("toggleButton");
 const navLink = document.getElementById("navLink");
@@ -7,11 +7,58 @@ toggleButton.addEventListener("click", () => {
 })
 
 // Project Section: tab
-const tabs = document.querySelectorAll('[data-tab-target');
-tabs.forEach(tab =>{
-    tab.addEventListener("click", () => {
-        const target = document.querySelector(tab.dataset.tabTarget);
-        // show target 
-        target.classList.add("active");
-    })
+// const tabs = document.querySelector(".wrapper");
+const tabButton = document.querySelectorAll(".tab-button");
+const tabsButtonWrapper = document.querySelector('.buttonWrapper')
+const contents = document.querySelectorAll(".content");
+const contentWrapper = document.querySelector('.contentWrapper');
+const flexContainer = document.querySelectorAll('.flexbox-item');
+// const frontendcontent = document.querySelectorAll('.flexbox-item');
+// console.log(tabButton);- Working
+// console.log(contents);
+// console.log(tabsButtonWrapper);
+
+tabsButtonWrapper.addEventListener('click', function(e) {
+  // clicked variable assigned to the buttons tabs
+  const clicked = e.target;
+  console.log(clicked);
+  if(!clicked)
+    return false;
+  // remove active in button
+  tabButton.forEach(tab => tab.classList.remove('active'))
+  // remove active in contents of tab
+  contents.forEach(content => content.classList.remove('active'));
+  // add active in button
+  clicked.classList.add('active');
+  const element = document.getElementById(clicked.dataset.id);
+  element.classList.add("active");
+});
+
+// Scroll Up button will show when scrolling 
+const showOnPx = 100;
+const btnScrollToUp = document.querySelector('#btnScrollToUp');
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+}
+
+document.addEventListener('scroll', function () {
+  if(scrollContainer().scrollTop > showOnPx) {
+    btnScrollToUp.style.display = 'block';
+  } else {
+    btnScrollToUp.style.display = 'none';
+  }
 })
+// Click on the scroll up button and will move to the top
+btnScrollToUp.addEventListener('click', function () {
+  // plain JS
+  // window.scrollTo(0, 0);
+
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior:'smooth'
+  })
+  // JQuery- Not working
+  // $("html, body").animate({scrollTop: 0}, "slow");
+});
